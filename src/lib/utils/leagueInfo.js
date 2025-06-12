280ff1f94140a88a51fdfc223e812a1ade7c3f9e
moose735 <132318332+moose735@users.noreply.github.com>
2025-06-12 09:28:49 -0400

    Create leagueInfo.js

---
+++ src/lib/utils/leagueInfo.js
@@ -0,0 +1,83 @@
+/*   STEP 1   */
+export const leagueID = "1048371694643060736"; // your league ID
+export const leagueName = "The League of Extraordinary Douchebags"; // your league name
+export const dues = 60; // (optional) used in template constitution page
+export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
+export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
+
+/*   STEP 2   */
+export const homepageText = `
+  <p>Welcome to The League of Extraordinary Douchebags (TLOED). This league was originally created as a </p>
+  <p>redraft league in 2018 at St. Bonaventure University by St. Bonaventure students. In 2021, the league was</p>
+  <p>rebranded into its current keeper league form; 12 teams, 3 divisions, 3 keepers per team. This is</p>
+  <p>considered a money league, and all owners are required to pay their league dues before the draft </p>
+  <p>commences. This is a highly-competitive league that encourages trash-talk and taunting, but rules of the</p>
+  <p>league need to be followed. The rules of the league are outlined below and are intended to keep the </p>
+  <p>league competitive, fair, and in existence for the foreseeable future.</p>
+`;
+
+/*   STEP 3   */
+/*
+3 managers as an example. Uncomment (remove the //) before each line to make it live code
+If you're having trouble, reference the Training Wheels' Manager Section
+https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding-managers-and-changing-the-homepage-text
+*/
+
+// To omit an optional field, set it's value to null
+
+export const managers = [
+    {
+      "roster": 783789669597999104,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "name": "Jon Boilard",
+      "location": "Brooklyn", // (optional)
+      "bio": "Lorem ipsum...",
+      "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+      "fantasyStart": 2020, // (optional) when did the manager start playing fantasy football
+      "favoriteTeam": "jax", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+      "rival": {
+        name: "OD", // Can be anything (usually your rival's name)
+        link: 6, // manager array number within this array, or null to link back to all managers page
+        image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+      },
+      "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+      "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
+      "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
+      "philosophy": "Your fantasy team's philosophy",
+      "tradingScale": 10, // 1 - 10
+      "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
+    },
+  ]
+  
+  
+  /*   !!  !!  IMPORTANT  !!  !! */
+  /*
+  Below is the most up to-date version of a manager. Please leave this commented out
+  and don't delete it. This will be updated if any fields are added, removed or changed
+  and will allow updates without causing merge conflicts
+  */
+  
+    // {
+    //   "roster": 3,  // (DEPRECATED! Don't use this anymore) ID of the roster that the manager manages (look at the order of the power rankings graph)
+    //   "managerID": "12345678",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
+    //   "name": "Your Name",
+    //   "tookOver": 2020, // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
+    //   "location": "Brooklyn", // (optional)
+    //   "bio": "Lorem ipsum...",
+    //   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+    //   "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
+    //   "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+    //   "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+    //   "rival": {
+    //     name: "Rival", // Can be anything (usually your rival's name)
+    //     link: 6, // manager array number within this array, or null to link back to all managers page
+    //     image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+    //   },
+    //   "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+    //   "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
+    //   "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
+    //   "philosophy": "Your fantasy team's philosophy", // (optional)
+    //   "tradingScale": 10, // 1 - 10 (optional)
+    //   "preferredContact": "Text",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
+    // },
+    
